ba149d1a764d4df0491cf5a8a2a622ad4e8a040b
Kartavya Shankar <kartavyashankar@gmail.com>
2021-03-14 01:59:37 +0530

    some more minor changes

--- code.js
+++ code.js
@@ -5,26 +5,26 @@ const u1 = document.getElementById("u1");
 const c1 = document.getElementById("c1");
 
 function openPanel() {
-	var mySpan = document.getElementById("p1");
-	mySpan.style.width = "30%";
-	mySpan.style.padding = "2% 5% 2% 2%";
-	document.getElementById("bn").style.display = "none";
+    var mySpan = document.getElementById("p1");
+    mySpan.style.width = "350px";
+    mySpan.style.padding = "2% 5% 2% 2%";
+    document.getElementById("bn").style.display = "none";
 }
 
 function closePanel() {
-	var mySpan = document.getElementById("p1");
-	mySpan.style.width = "0";
-	mySpan.style.padding = "0";
-	document.getElementById("bn").style.display = "block";
+    var mySpan = document.getElementById("p1");
+    mySpan.style.width = "0";
+    mySpan.style.padding = "0";
+    document.getElementById("bn").style.display = "block";
 }
 
 function togglenform() {
-	var nform = document.getElementById("newMeme");
-	if(nform.style.height === "0px") {
-		nform.style.height = "50%";
-	} else {
-		nform.style.height = "0";
-	}
+    var nform = document.getElementById("newMeme");
+    if (nform.style.height === "0px") {
+        nform.style.height = "50%";
+    } else {
+        nform.style.height = "0";
+    }
 }
 
 var meme_id;
@@ -32,84 +32,91 @@ var meme_id;
 var editForm = document.getElementById("patchForm");
 
 function closewin() {
-	editForm.style.display = "none";
+    editForm.style.display = "none";
 }
 
 function loadForm(id) {
-	meme_id = id;
-	editForm.style.display = "block";
+    meme_id = id;
+    editForm.style.display = "block";
 }
 
 function fetchFullData() {
-	fetch(target).then((res) => {
-		return res.json();
-	}).then((data) => {
-		data.forEach((meme) => {
-			container.insertAdjacentHTML("afterbegin", 
-				`<div class="meme">
-					<button class="edit" style="float: right;" onclick="loadForm('${meme._id}')"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm_ejkBqA8WWNyPM3dEmRJr-C3yRRoU_08QA&usqp=CAU" style="height: 30px; width: 30px;" id='${meme._id}'></button>
+    fetch(target)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            data.forEach((meme) => {
+                container.insertAdjacentHTML(
+                    "afterbegin",
+                    `<div class="meme">
+					<button class="edit" style="float: right;" onclick="loadForm('${meme._id}')"><img src="https://www.clipartmax.com/png/middle/241-2411058_edit-button-icon-png.png" style="height: 30px; width: 30px;" id='${meme._id}'></button>
 					<center><p class="cap">"${meme.caption}"</p>
 					<img src="${meme.url}" alt="${meme.caption}"></center>
 					<p  class="uname">Added By: ${meme.name}</p>
-				 </div>`);
-		});
-	}).catch((err) => {
-		window.alert(err);
-	});
+				 </div>`
+                );
+            });
+        })
+        .catch((err) => {
+            window.alert(err);
+        });
 }
 
-editForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-	var formData = new FormData(e.currentTarget);
-	var value = Object.fromEntries(formData.entries());
-	if(value.url === "") {
-		delete value.url;
-	}
-	if(value.caption === "") {
-		delete value.caption;
-	}
-	fetch(target+`/${meme_id}`, {
-		method: 'PATCH',
-		body: JSON.stringify(value),
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	}).then((res) => {
-		container.innerHTML="";
-
-	}).catch((err) => {
-		window.alert(error);
-	});
-	document.querySelectorAll('.out').forEach((query) => {
-		query.value="";
-	});
-	closewin();
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var formData = new FormData(e.currentTarget);
+    var value = Object.fromEntries(formData.entries());
+    if (value.url === "") {
+        delete value.url;
+    }
+    if (value.caption === "") {
+        delete value.caption;
+    }
+    fetch(target + `/${meme_id}`, {
+            method: "PATCH",
+            body: JSON.stringify(value),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((res) => {
+            container.innerHTML = "";
+        })
+        .catch((err) => {
+            window.alert(error);
+        });
+    document.querySelectorAll(".out").forEach((query) => {
+        query.value = "";
+    });
+    closewin();
 });
 
 var newForm = document.getElementById("newForm");
 
-newForm.addEventListener('submit', (event) => {
+newForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-	event.preventDefault();
-
-	const formData = new FormData(event.currentTarget);
-	var value =  Object.fromEntries(formData.entries());
-	console.log(value);
-	fetch(target, {
-		method: 'POST',
-		body: JSON.stringify(value),
-		headers: {
-          'Content-Type': 'application/json'
-        }
-	}).then((res) => {
-		container.innerHTML = "";
-		fetchFullData();
-	}).catch((err) => {
-		window.alert("Error: " + err);
-	});
-	n1.value = "";
-	u1.value = "";
-	c1.value = "";
+    const formData = new FormData(event.currentTarget);
+    var value = Object.fromEntries(formData.entries());
+    console.log(value);
+    fetch(target, {
+            method: "POST",
+            body: JSON.stringify(value),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((res) => {
+            container.innerHTML = "";
+            fetchFullData();
+        })
+        .catch((err) => {
+            window.alert("Error: " + err);
+        });
+    n1.value = "";
+    u1.value = "";
+    c1.value = "";
 });
 
 fetchFullData();
